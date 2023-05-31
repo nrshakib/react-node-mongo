9a73d5497837475effdd4372b5da103d526f62d2
NRS\nrsha <nrshakib007@gmail.com>
2023-05-31 14:47:44 +0600

    added user form

--- ClientSide/src/components/AddUser/AddUser.jsx
+++ ClientSide/src/components/AddUser/AddUser.jsx
@@ -1,7 +1,34 @@
 const AddUser = () => {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
+    const user = { name, email };
+
+    //send data to server
+  //   fetch("http://localhost:5000/user", {
+  //     method: "POST",
+  //     headers: {
+  //       "content-type": "application/json",
+  //     },
+  //     body: JSON.stringify(user),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log("success", data);
+  //     });
+  // };
   return (
     <div>
       <h2>Add A New User</h2>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" required />
+        <br />
+        <input type="email" name="email" placeholder="E-Mail" required />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </div>
   );
 };
